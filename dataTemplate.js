f83bcbf8383cf92290c0c0cba05f1303b41a9ee4
Hussain <YHussai1@HFHS.ORG>
2018-06-16 18:39:47 -0400

    first Commit

---
+++ dataTemplate.js
@@ -0,0 +1,21 @@
+console.log("Herro");
+
+var tester = {
+	name: "Steve",
+	species: "dog",
+	age: 5
+};
+
+console.log(tester);
+
+var teams = [];
+
+console.log(teams);
+
+var ateam = {
+	name: "Lions",
+	city: "Detroit"
+};
+
+teams.push (ateam);
+console.log(teams);
